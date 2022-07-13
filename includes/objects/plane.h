5beaae6cc4d689cfafea7ce52de0eec4f8ca6804
dann-romm <danilka.2002.lol@gmail.com>
2022-07-13 18:54:18 +0300

    plane header added

---
+++ includes/objects/plane.h
@@ -0,0 +1,41 @@
+#ifndef PLANE_H
+# define PLANE_H
+
+# include "object.h"
+# include "bool.h"
+
+typedef struct s_plane
+{
+	t_point3d	point;
+	t_vector3d	normal;
+	t_color		color;
+	t_material	material;
+}	t_plane;
+
+// initialize plane with given values
+t_object3d	*new_plane(t_point3d point, t_vector3d normal, t_color color, t_material material);
+
+// check if the ray intersects the object
+// return TRUE if the ray intersects the object, FALSE otherwise
+// the intersection point is stored in the intersection_point parameter
+t_bool		intersect_plane(const void *data, const t_point3d vector_start, const t_vector3d vector, t_point3d *intersection_point);
+
+// get color at given point
+t_color		get_color_plane(const void *data, const t_point3d intersection_point);
+
+// get the normal vector at the given point
+t_vector3d	get_normal_vector_plane(const void *data, const t_point3d intersection_point);
+
+// get the material at given point
+t_material	get_material_plane(const void *data, const t_point3d intersection_point);
+
+// get minimum point of bounding box of object
+t_point3d	get_min_boundary_point_plane(const void *data);
+
+// get maximum point of bounding box of object
+t_point3d	get_max_boundary_point_plane(const void *data);
+
+// release memory allocated by object
+void		release_data_plane(void *data);
+
+#endif // PLANE_H
