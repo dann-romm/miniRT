cb80607447add2e4ea31ba8c171dbb8a7331d3f4
dann-romm <danilka.2002.lol@gmail.com>
2022-10-15 16:27:38 +0300

    ok

--- librender/src/render/render.c
+++ librender/src/render/render.c
@@ -6,16 +6,18 @@
 
 double	calc_divisor(const t_camera *camera, const t_canvas *canvas)
 {
-	double divisor;
+	double	div;
 
-	divisor = canvas->w / (2.0 * camera->proj_plane_dist * tan(camera->fov / 2));
-	return divisor;
+	div = canvas->w / (2.0 * camera->proj_plane_dist * tan(camera->fov / 2));
+	return (div);
 }
 
-void	render_scene(const t_scene *const scene, const t_camera *const camera, t_canvas *canvas, const int num_threads)
+void	render_scene(const t_scene *const scene,
+	const t_camera *const camera, t_canvas *canvas, const int num_threads)
 {
 	const double	dx = canvas->w / 2.0;
 	const double	dy = canvas->h / 2.0;
+	const double	divisor = calc_divisor(camera, canvas);
 	int				i;
 	int				j;
 	double			x;
@@ -23,9 +25,6 @@ void	render_scene(const t_scene *const scene, const t_camera *const camera, t_ca
 	t_vector3d		ray;
 	t_color			col;
 
-	// TODO: consider possibility to define these OpenMP parameters
-	// in declarative style (using directives of preprocessor)
-	double divisor = calc_divisor(camera, canvas);
 	i = 0;
 	while (i < canvas->w)
 	{
@@ -41,36 +40,4 @@ void	render_scene(const t_scene *const scene, const t_camera *const camera, t_ca
 		}
 		i++;
 	}
-	
-	// // TODO: argument of the function? global variable?
-	// const int antialiasing = ANTIALIASING;
-	
-	// if (antialiasing)
-	// {
-	// 	t_canvas * edges = detect_edges_canvas(canvas, num_threads);
-	// 	for(i = 1; i < w - 1; i++) {
-	// 		for(j = 1; j < h - 1; j++) {
-	// 			// edges canvas is grayscaled
-	// 			// it means that color components (r, g, b) are equal
-	// 			Byte gray = get_pixel(i, j, edges).r;
-			
-	// 			// TODO: improve
-	// 			if(gray > 10) {
-	// 				const double x = i - dx;
-	// 				const double y = j - dy;
-				
-	// 				t_color c = get_pixel(i, j, canvas);
-				
-	// 				const double weight = 1.0 / 4;
-	// 				c = mul_color(c, weight);
-	// 				c = add_colors(c, mul_color(trace(scene, camera, vector3df(x + 0.5, y, focus)), weight));
-	// 				c = add_colors(c, mul_color(trace(scene, camera, vector3df(x, y + 0.5, focus)), weight));
-	// 				c = add_colors(c, mul_color(trace(scene, camera, vector3df(x + 0.5, y + 0.5, focus)), weight));
-				
-	// 				set_pixel(i, j, c, canvas);
-	// 			}
-	// 		}
-	// 	}
-	// 	release_canvas(edges);
-	// }
 }
--- librender/src/render/tracer.c
+++ librender/src/render/tracer.c
@@ -15,37 +15,37 @@
 // --------------------------------------------------------------
 
 t_color	trace_recursively(
-	const t_scene *const scene,
-	const t_point3d vector_start,
-	const t_vector3d vector,
-	const double intensity,
-	const int recursion_level);
+			const t_scene *const scene,
+			const t_point3d vector_start,
+			const t_vector3d vector,
+			const double intensity,
+			const int recursion_level);
 
 t_bool	is_viewable(
-	const t_point3d target_point,
-	const t_point3d starting_point,
-	const t_scene *const scene);
+			const t_point3d target_point,
+			const t_point3d starting_point,
+			const t_scene *const scene);
 
-t_color get_lighting_color(
-	const t_point3d point,
-	const t_vector3d norm_v,
-	const t_scene *const scene);
+t_color	get_lighting_color(
+			const t_point3d point,
+			const t_vector3d norm_v,
+			const t_scene *const scene);
 
 t_color	get_specular_color(
-	const t_point3d point,
-	const t_vector3d reflected_ray,
-	const t_scene *const scene,
-	const double p);
+			const t_point3d point,
+			const t_vector3d reflected_ray,
+			const t_scene *const scene,
+			const double p);
 
 t_color	calculate_color(
-	const t_scene *const scene,
-	const t_point3d vector_start,
-	const t_vector3d vector,
-	t_object3d *const * obj_ptr,
-	const t_point3d *const point_ptr,
-	const double *const dist_ptr,
-	const double intensity,
-	const int recursion_level);
+			const t_scene *const scene,
+			const t_point3d vector_start,
+			const t_vector3d vector,
+			t_object3d *const *obj_ptr,
+			const t_point3d *const point_ptr,
+			const double *const dist_ptr,
+			const double intensity,
+			const int recursion_level);
 
 // reflect_ray.c
 t_vector3d	reflect_ray(const t_vector3d incident_ray, const t_vector3d norm_v);
@@ -56,18 +56,17 @@ t_vector3d	reflect_ray(const t_vector3d incident_ray, const t_vector3d norm_v);
 t_color	trace(const t_scene *const scene,
 	const t_camera *const camera, t_vector3d vector)
 {
-	double tmp = vector.x;
+	double		tmp;
+	t_vector3d	r_vector;
+
+	tmp = vector.x;
 	vector.x = -vector.y;
 	vector.y = tmp;
-
-	t_vector3d r_vector = rotate_vector_by_matrix(vector, camera->rotation_matrix);
+	r_vector = rotate_vector_by_matrix(vector, camera->rotation_matrix);
 	return (trace_recursively(scene, camera->camera_position,
-		r_vector, INITIAL_RAY_INTENSITY, 0));
+			r_vector, INITIAL_RAY_INTENSITY, 0));
 }
 
-// possibly - redundant code (was added to prevent overflow of double)
-// TODO: remove
-//normalize_vector(&vector);
 t_color	trace_recursively(const t_scene *const scene,
 	const t_point3d vector_start, const t_vector3d vector,
 	const double intensity, const int recursion_level)
@@ -75,12 +74,12 @@ t_color	trace_recursively(const t_scene *const scene,
 	t_object3d	*nearest_obj;
 	t_point3d	nearest_intersection_point;
 	double		nearest_intersection_point_dist;
-	
+
 	nearest_obj = NULL;
 	nearest_intersection_point_dist = DBL_MAX;
 	if (find_intersection_tree(scene->kd_tree, vector_start, vector,
-		&nearest_obj, &nearest_intersection_point,
-		&nearest_intersection_point_dist))
+			&nearest_obj, &nearest_intersection_point,
+			&nearest_intersection_point_dist))
 	{
 		return (calculate_color(scene, vector_start, vector, &nearest_obj,
 				&nearest_intersection_point, &nearest_intersection_point_dist,
@@ -89,103 +88,89 @@ t_color	trace_recursively(const t_scene *const scene,
 	return (scene->background_color);
 }
 
-t_color	calculate_color(const t_scene *const scene, const t_point3d vector_start, const t_vector3d vector, t_object3d *const *obj_ptr, const t_point3d *const point_ptr, const double *const dist_ptr, const double intensity, const int recursion_level)
+t_color	calculate_color(const t_scene *const scene,
+	const t_point3d vector_start, const t_vector3d vector,
+	t_object3d *const *obj_ptr, const t_point3d *const point_ptr,
+	const double *const dist_ptr, const double intensity,
+	const int recursion_level)
 {
 	const t_object3d	*obj = *obj_ptr;
 	const t_point3d		point = *point_ptr;
 	const double		dist = *dist_ptr;
 	const t_material	material = obj->get_material(obj->data, point);
 	const t_vector3d	norm = obj->get_normal_vector(obj->data, point);
+	t_vector3d			reflected_ray;
+	t_color				light_color;
+	t_color				obj_color;
+	t_color				ambient_color;
+	t_color				diffuse_color;
+	t_color				reflected_color;
+	t_color				specular_color;
+	t_color				result;
 
-	t_color	obj_color;
-	t_color	ambient_color;
-	t_color	diffuse_color;
-	t_color	reflected_color;
-	t_color	specular_color;
 	obj_color = obj->get_color(obj->data, point);
-
-	double	fog_density;
-	fog_density = 0;
-
-	t_vector3d	reflected_ray;
 	if ((material.Ks) || (material.Kr))
 		reflected_ray = reflect_ray(vector, norm);
-	
-	// Ambient
 	if (material.Ka)
 		ambient_color = mix_colors(scene->background_color, obj_color);
-	
-	// Diffuse
 	if (material.Kd)
 	{
 		diffuse_color = obj_color;
 		if (scene->light_sources_len)
 		{
-			t_color light_color = get_lighting_color(point, norm, scene);
+			light_color = get_lighting_color(point, norm, scene);
 			diffuse_color = mix_colors(diffuse_color, light_color);
 		}
 	}
-	
-	// Specular
 	if (material.Ks)
 	{
 		specular_color = scene->background_color;
 		if (scene->light_sources_len)
-			specular_color = get_specular_color(point, reflected_ray, scene, material.p);
+			specular_color = get_specular_color(point,
+					reflected_ray, scene, material.p);
 	}
-	
-	// Reflect
 	if (material.Kr)
 	{
-		// Avoid deep recursion by tracing rays, which have intensity is greather than threshold
-		// and avoid infinite recursion by limiting number of recursive calls
 		if ((intensity > THRESHOLD_RAY_INTENSITY)
 			&& (recursion_level < MAX_RAY_RECURSION_LEVEL))
 		{
-			reflected_color = trace_recursively(scene, point, reflected_ray, intensity * material.Kr * (1 - fog_density), recursion_level + 1);
+			reflected_color = trace_recursively(scene, point, reflected_ray,
+					intensity * material.Kr, recursion_level + 1);
 		}
 		else
 		{
 			reflected_color = scene->background_color;
 		}
 	}
-	
-	// Result
-	t_color	result_color = rgb(0, 0, 0);
-
+	result = rgb(0, 0, 0);
 	if (material.Ka)
-		result_color = add_colors(result_color, mul_color(ambient_color, material.Ka));
+		result = add_colors(result, mul_color(ambient_color, material.Ka));
 	if (material.Kd)
-		result_color = add_colors(result_color, mul_color(diffuse_color, material.Kd));
+		result = add_colors(result, mul_color(diffuse_color, material.Kd));
 	if (material.Ks)
-		result_color = add_colors(result_color, mul_color(specular_color, material.Ks));
+		result = add_colors(result, mul_color(specular_color, material.Ks));
 	if (material.Kr)
-		result_color = add_colors(result_color, mul_color(reflected_color, material.Kr));
-
-	return (result_color);
+		result = add_colors(result, mul_color(reflected_color, material.Kr));
+	return (result);
 }
 
-t_color	get_lighting_color(const t_point3d point, const t_vector3d norm_v, const t_scene *const scene)
+t_color	get_lighting_color(const t_point3d point,
+	const t_vector3d norm_v, const t_scene *const scene)
 {
-	// possibly - redundant code (was added to prevent overflow of double)
-	// TODO: remove
-	//normalize_vector(&norm_v);
-
-	t_color				light_color = rgb(0, 0, 0);
+	t_color				light_color;
 	t_light_source3d	*ls;
 	t_vector3d			v_ls;
 	double				cos_ls;
 	t_color				color_ls;
 	int					i;
 
+	light_color = rgb(0, 0, 0);
 	i = 0;
 	while (i < scene->light_sources_len)
 	{
 		if (scene->light_sources[i])
 		{
 			ls = scene->light_sources[i];
-
-			// If not shaded
 			if (is_viewable(ls->location, point, scene))
 			{
 				v_ls = vector3dp(point, ls->location);
@@ -199,22 +184,23 @@ t_color	get_lighting_color(const t_point3d point, const t_vector3d norm_v, const
 	return (light_color);
 }
 
-t_color	get_specular_color(const t_point3d point, const t_vector3d reflected_ray, const t_scene *const scene, const double p)
+t_color	get_specular_color(const t_point3d point,
+	const t_vector3d reflected_ray, const t_scene *const scene, const double p)
 {
-	t_color				light_color = rgb(0, 0, 0);
+	t_color				light_color;
 	t_light_source3d	*ls;
 	t_vector3d			v_ls;
 	double				cos_ls;
 	t_color				color_ls;
 	int					i;
-	
+
+	light_color = rgb(0, 0, 0);
 	i = 0;
 	while (i < scene->light_sources_len)
 	{
-		if (scene->light_sources[i]) {
+		if (scene->light_sources[i])
+		{
 			ls = scene->light_sources[i];
-		
-			// If not shaded
 			if (is_viewable(ls->location, point, scene))
 			{
 				v_ls = vector3dp(point, ls->location);
@@ -228,23 +214,25 @@ t_color	get_specular_color(const t_point3d point, const t_vector3d reflected_ray
 		}
 		i++;
 	}
-	
 	return (light_color);
 }
 
-t_bool	is_viewable(const t_point3d target_point, const t_point3d starting_point, const t_scene *const scene)
+t_bool	is_viewable(const t_point3d target_point,
+	const t_point3d starting_point, const t_scene *const scene)
 {
 	const t_vector3d	ray = vector3dp(starting_point, target_point);
 	const double		target_dist = module_vector(ray);
-	t_object3d			*nearest_obj = NULL;
+	t_object3d			*nearest_obj;
 	t_point3d			nearest_intersection_point;
-	double				nearest_intersection_point_dist = DBL_MAX;
-	
-	if (find_intersection_tree(scene->kd_tree, starting_point, ray, &nearest_obj, &nearest_intersection_point, &nearest_intersection_point_dist))
+	double				nearest_intersection_point_dist;
+
+	nearest_obj = NULL;
+	nearest_intersection_point_dist = DBL_MAX;
+	if (find_intersection_tree(scene->kd_tree, starting_point,
+			ray, &nearest_obj, &nearest_intersection_point,
+			&nearest_intersection_point_dist))
 	{
-		// Check if intersection point is closer than target_point
 		return (target_dist < nearest_intersection_point_dist);
 	}
-	// Ray doesn't intersect any of scene objects
 	return (1);
 }
